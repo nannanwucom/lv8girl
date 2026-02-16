af579c455ad29ec3fb8a6a39478d86e1fe3b3e81
MCHappyFriskYo <154609131+MCHappyFriskYo@users.noreply.github.com>
2026-02-17 02:27:17 +0800

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1153,14 +1153,14 @@ $username = $_SESSION['username'] ?? '';
 
         <!-- 底部 -->
         <footer class="footer">
-            <div>© 2025 lv8girl · 绿坝娘二次元社区</div>
+            <div><a href="https://icp.gov.moe/?keyword=20260911" target="_blank" style="text-decoration:none;color:red>萌ICP备20260911号</a></div>
             <div class="footer-links">
                 <a href="#">关于</a>
                 <a href="#">帮助</a>
                 <a href="#">隐私</a>
                 <a href="#">投稿</a>
             </div>
-            <a href="https://icp.gov.moe/?keyword=20260911" target="_blank" style="text-decoration:none;color:red>萌ICP备20260911号</a>
+            
         </footer>
     </div>
 
@@ -1175,4 +1175,5 @@ $username = $_SESSION['username'] ?? '';
 </body>
 
 </html>
+
 
