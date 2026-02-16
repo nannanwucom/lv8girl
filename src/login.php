4e62f4db8651c734e1402a7822beca07307a12a5
喃喃污 <mail@nannanwu.com>
2026-02-17 02:24:52 +0800

    Refactor login logic and UI in login.php

--- src/login.php
+++ src/login.php
@@ -329,7 +329,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                     <input type="text" name="login" placeholder="请输入用户名或邮箱" required value="<?php echo isset($_POST['login']) ? htmlspecialchars($_POST['login']) : ''; ?>">
                 </div>
                 <div class="form-group">
-                    <label>密码测试</label>
+                    <label>密码</label>
                     <input type="password" name="password" placeholder="请输入密码" required>
                 </div>
                 <div class="forgot-password">
@@ -354,3 +354,4 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 </body>
 
 </html>
+
